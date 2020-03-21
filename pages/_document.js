2a68f661b1c333527824aea10421fa670f38747f
Gavyn McKenzie <gavyn@etchapps.com>
2020-03-21 14:55:49 +0000

    feat: Meta and OG tags

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,20 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta charset="utf-8" />
+          <title>Abi's Remote Birthday</title>
+          <meta name="description" content="Video hangout, Netflix Party, Fried Chicken, FUCK MILEY!" />
+          <meta name="image" content="https://abi-birthday.now.sh/share-card.png" />
+          <meta itemprop="name" content="Abi's Remote Birthday" />
+          <meta itemprop="description" content="Video hangout, Netflix Party, Fried Chicken, FUCK MILEY!" />
+          <meta itemprop="image" content="https://abi-birthday.now.sh/share-card.png" />
+          <meta name="og:title" content="Abi's Remote Birthday" />
+          <meta name="og:description" content="Video hangout, Netflix Party, Fried Chicken, FUCK MILEY!" />
+          <meta name="og:image" content="https://abi-birthday.now.sh/share-card.png" />
+          <meta name="og:url" content="https://abi-birthday.now.sh" />
+          <meta name="og:site_name" content="Abi's Remote Birthday" />
+          <meta name="og:locale" content="en_UK" />
+          <meta name="og:type" content="website" />
           <link href="https://fonts.googleapis.com/css?family=Merriweather:400,700&display=swap" rel="stylesheet" />
         </Head>
         <body>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Youtube from 'react-youtube';
 
 import Grid from '../components/composition/Grid';
 import Card from '../components/canvas/Card';
@@ -32,91 +31,78 @@ import Img from '../components/content/Img';
 
 export default function Home() {
   return (
-    <Grid>
-      <Grid.Item>
-        <Img src="/abi-birthday.png" alt="Abi's last birthday" />
-      </Grid.Item>
-      <Grid.Item>
-        <Card theme="blue">
-          <h1>Abi's Remote Birthday</h1>
-        </Card>
-      </Grid.Item>
-      <Grid.Item>
-        <Img src="/abi-mexican.png" alt="Abi being a mexican" />
-      </Grid.Item>
-      <Grid.Item>
-        <Card theme="red">
-          <h2>Video Hangout</h2>
-          <p>6:30pm</p>
-          <p>
-            <a href="https://hangouts.google.com/call/DJ3L6zMHE0VE5AqjimGRAEEI" target="_blank" rel="noopener noreferrer">
-              Join the call
-            </a>
-          </p>
-        </Card>
-      </Grid.Item>
-      <Grid.Item>
-        <Img src="/abi.png" alt="Abi" />
-      </Grid.Item>
-      <Grid.Item>
-        <Card theme="green">
-          <h2>NetFlix Party</h2>
-          <p>7pm</p>
-          <p>
-            <a href="https://www.netflix.com/watch/60020917?tctx=0%2C0%2C%2C%2C" target="_blank" rel="noopener noreferrer">
-              Watch the movie
-            </a>
-          </p>
-        </Card>
-      </Grid.Item>
-
-      <Grid.Item>
-        <Img src="/abi-nye.png" alt="Abi on NYE" />
-      </Grid.Item>
-
-      <Grid.Item>
-        <Card theme="aqua">
-          <h2>Recommended food</h2>
-          <p>Fried chicken</p>
-          <p>
-            <a href="https://www.just-eat.co.uk/takeaway/nearme/chicken" target="_blank" rel="noopener noreferrer">
-              Order chicken
-            </a>
-          </p>
-        </Card>
-      </Grid.Item>
-
-      <Grid.Item>
-        <Img src="/abi-underwater.png" alt="Abi is underwater" />
-      </Grid.Item>
-
-      <Grid.Item>
-        <Card theme="black">
-          <h2>What now?</h2>
-          <p>Start getting excited</p>
-          <p>
-            <a href="https://www.youtube.com/watch?v=VuKGHpWffiQ" target="_blank" rel="noopener noreferrer">
-              Watch the trailer
-            </a>
-          </p>
-        </Card>
-      </Grid.Item>
-
-      {/*
-
+    <>
+      <Grid>
         <Grid.Item>
-          <Img src="/luck-dragon.png" alt="Always trust to luck" />
+          <Img src="/abi-birthday.png" alt="Abi's last birthday" />
+        </Grid.Item>
+        <Grid.Item>
+          <Card theme="blue">
+            <h1>Abi's Remote Birthday</h1>
+          </Card>
+        </Grid.Item>
+        <Grid.Item>
+          <Img src="/abi-mexican.png" alt="Abi being a mexican" />
+        </Grid.Item>
+        <Grid.Item>
+          <Card theme="red">
+            <h2>Video Hangout</h2>
+            <p>6:30pm</p>
+            <p>
+              <a href="https://hangouts.google.com/call/DJ3L6zMHE0VE5AqjimGRAEEI" target="_blank" rel="noopener noreferrer">
+                Join the call
+              </a>
+            </p>
+          </Card>
+        </Grid.Item>
+        <Grid.Item>
+          <Img src="/abi.png" alt="Abi" />
+        </Grid.Item>
+        <Grid.Item>
+          <Card theme="green">
+            <h2>NetFlix Party</h2>
+            <p>7pm</p>
+            <p>
+              <a href="https://www.netflix.com/watch/60020917?tctx=0%2C0%2C%2C%2C" target="_blank" rel="noopener noreferrer">
+                Watch the movie
+              </a>
+            </p>
+          </Card>
         </Grid.Item>
 
         <Grid.Item>
-          <Img src="/abi-sparkles.png" alt="Abi wearing sparkles" />
+          <Img src="/abi-nye.png" alt="Abi on NYE" />
         </Grid.Item>
+
         <Grid.Item>
-          <Img src="/neverending-story.png" alt="The NeverEnding Story poster" />
+          <Card theme="aqua">
+            <h2>Recommended food</h2>
+            <p>Fried chicken</p>
+            <p>
+              <a href="https://www.just-eat.co.uk/takeaway/nearme/chicken" target="_blank" rel="noopener noreferrer">
+                Order chicken
+              </a>
+            </p>
+          </Card>
         </Grid.Item>
 
-      */}
+        <Grid.Item>
+          <Img src="/abi-underwater.png" alt="Abi is underwater" />
+        </Grid.Item>
 
-    </Grid>
+        <Grid.Item>
+          <Card theme="black">
+            <h2>What now?</h2>
+            <p>Start getting excited</p>
+            <p>
+              <a href="https://www.youtube.com/watch?v=VuKGHpWffiQ" target="_blank" rel="noopener noreferrer">
+                Watch the trailer
+              </a>
+            </p>
+          </Card>
+        </Grid.Item>
+
+      </Grid>
+    </>
   );
 }
